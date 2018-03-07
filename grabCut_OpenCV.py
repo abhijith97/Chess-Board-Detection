936a4aa014588d43f5f052e181d6c541534bc0bd
Abhijith <abhijith97@yahoo.co.in>
2018-03-08 02:06:02 +0530

    Add GrabCut sample code

---
+++ grabCut_OpenCV.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('Images/me2.jpg')
+mask = np.zeros(img.shape[:2],np.uint8)
+bgdModel = np.zeros((1,65),np.float64)
+fgdModel = np.zeros((1,65),np.float64)
+rect = (50,50,1155,1200)
+cv.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv.GC_INIT_WITH_RECT)
+mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+img = img*mask2[:,:,np.newaxis]
+plt.imshow(img),plt.show()
